3ea83db66aca66ee8eef3192a3510f1862d71931
masatohori <masatohori76@gmail.com>
2020-12-31 00:06:28 +0900

    ci 2

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'test_db'),
-            'username' => env('DB_USERNAME', 'user'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
